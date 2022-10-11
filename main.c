7b2c0f25baeddaa6650cbe34d1c2a897255871b7
mj0717 <a81930875@gmail.com>
2022-10-12 01:31:14 +0900

    6-3

--- main.c
+++ main.c
@@ -3,50 +3,37 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int combination(int x, int y)
+int sumTwo(int a, int b)
 {
-	return(factorial(x)/(factorial(x-y)*factorial(y)));
+	int result;
+	
+	result=a+b;
+	
+	return result;
 }
 
-int factorial(int n)
+int square(int n)
 {
-	int i;
-	int res=1;
-	for(i=1;i<=n;i++)
-		res=res*i;
-	return res;
+	return (n*n);
 }
 
-int get_integer()
+int get_max(int x, int y)
 {
-	int m;
-	printf("숫자를 입력하세요: ");
-	scanf("%i",&m);
-	
-	return m;
-	
+	if (x>y)
+		return x;
+	else
+		return y;	
 }
+
 
 int main(int argc, char *argv[]) {
+	int res_sum, res_square, res_get_max;
 	
-	int a, b;
-	int res_factorial;
+	res_sum=sumTwo(2,3);
+	res_square=square(5);
+	res_get_max=get_max(15,86);	
 	
-	a=get_integer();
-	b=get_integer();
-	
-	res_factorial=factorial(a);
-	res_factorial=factorial(a-b);
-	res_factorial=factorial(b);
-	
-	printf("%d!=%i\n",a, factorial(a));
-	printf("%d!=%i\n",a-b, factorial(a-b));
-	printf("%d!=%i\n",b, factorial(b));
-	
-	
-	printf("C(%i,%i)=%i\n",a,b,combination(a,b));
-	
-	
+	printf("%i %i %i\n", res_sum, res_square, res_get_max );
 	
 	return 0;
 }
