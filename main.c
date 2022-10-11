01fc1e70436a0453f6c5866c1bba6cdf5cb8d4e1
mj0717 <a81930875@gmail.com>
2022-10-12 01:45:36 +0900

    6-4-1

--- main.c
+++ main.c
@@ -3,16 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int square(int a)
+void square(int a)
 {
-	return(a*a);
+ 	a=a*a;
 }
 
 
 int main(int argc, char *argv[]) {
 	
 	int a=2;
-	a=square(a);
+	square(a);
 	printf("a=%i\n",a);
 	
 	return 0;
