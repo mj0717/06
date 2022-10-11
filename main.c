6c2e0036a19025b1b6f79ad59b1d16ab09cf88d1
mj0717 <a81930875@gmail.com>
2022-10-12 01:26:41 +0900

    6-5

--- main.c
+++ main.c
@@ -3,22 +3,50 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
-
-void func(int x)
+int combination(int x, int y)
 {
-
-	printf("func x is at %p\n", &x);
-
+	return(factorial(x)/(factorial(x-y)*factorial(y)));
 }
 
+int factorial(int n)
+{
+	int i;
+	int res=1;
+	for(i=1;i<=n;i++)
+		res=res*i;
+	return res;
+}
 
+int get_integer()
+{
+	int m;
+	printf("숫자를 입력하세요: ");
+	scanf("%i",&m);
+	
+	return m;
+	
+}
 
 int main(int argc, char *argv[]) {
 	
-	int x;
-	printf("main x is at %p\n", &x);
-	func(x);
-		
+	int a, b;
+	int res_factorial;
+	
+	a=get_integer();
+	b=get_integer();
+	
+	res_factorial=factorial(a);
+	res_factorial=factorial(a-b);
+	res_factorial=factorial(b);
+	
+	printf("%d!=%i\n",a, factorial(a));
+	printf("%d!=%i\n",a-b, factorial(a-b));
+	printf("%d!=%i\n",b, factorial(b));
+	
+	
+	printf("C(%i,%i)=%i\n",a,b,combination(a,b));
+	
+	
+	
 	return 0;
 }
